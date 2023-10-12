53231b4be608d83a44f2d8cc173722e3bd450ee1
apaala.chatterjee <apaala.chatterjee@apaalam1-osx.local>
2023-10-12 14:02:28 -0400

    mbugs

--- QA.py
+++ QA.py
@@ -54,8 +54,8 @@ def main():
 
     #calc md5sum for each file and save to corresponding column
     for i in range(0, len(md5sums_df)):
-        tmp_md5sum = compute_md5(md5sums_df[i, 'File'])
-        md5sums_df[i,'calculated_md5sum' ] = tmp_md5sum
+        tmp_md5sum = compute_md5(md5sums_df.at[i, 'File'])
+        md5sums_df.set_value(i,'calculated_md5sum',tmp_md5sum)
     print(md5sums_df)
     
     
