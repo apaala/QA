ba168d41d3820da886f8e4a1f25b1b6c5dd6b050
apaala.chatterjee <apaala.chatterjee@apaalam1-osx.local>
2023-11-02 13:41:09 -0400

    incorporating test feedback

--- QA.py
+++ QA.py
@@ -109,12 +109,12 @@ def check_R1_R2_fastq(lane_files, lane):
     print(matches)
     #add logic for checking if matched, else error
     if matches:
-        logger.info(f"In check_R1_R2_fastq(). Following files for lane: {lane} passed: %s",",".join(ext_req_checked.filename))
+        logger.info(f"In check_R1_R2_fastq(). Following files for lane: {lane} passed: %s ",",".join(ext_req_checked.filename))
     else:
-        logger.error(f"In check_R1_R2_fastq(). Following files for lane: {lane} failed: %s",",".join(ext_req_checked.filename))
+        logger.error(f"In check_R1_R2_fastq(). Following files for lane: {lane} failed: %s ",",".join(ext_req_checked.filename))
     #logger.info("In check_R1_R2_fastq(). Following files for lane: {lane} passed: %s ",ext_req_checked)
     #temporary prints for new users. Will be replaced with logging.
-    print("In check_R1_R2_fastq(). Following files for lane: ", lane," passed: ",','.join(ext_req_checked.filename))
+    #print("In check_R1_R2_fastq(). Following files for lane: ", lane," passed: ",','.join(ext_req_checked.filename))
     return(ext_req_checked)
 
 def check_I1_I2_fastq(lane_files, lane):
@@ -131,9 +131,9 @@ def check_I1_I2_fastq(lane_files, lane):
         ext_req_checked = required_files[required_files['filename'].str.contains("fastq")]
         if len(required_files) == 2 and len(ext_req_checked) !=2:
             ext_req_checked = required_files[required_files['filename'].str.contains("fq")]
-    logger.info(f"In check_I1_I2_fastq(). Following files for lane: {lane} passed: %s ",ext_req_checked)
+    logger.info(f"In check_I1_I2_fastq(). Following files for lane: {lane} passed: %s ",",".join(ext_req_checked.filename))
     #temporary prints for new users. Will be replaced with logging.
-    print("In check_I1_I2_fastq(). Following files for lane: ", lane," passed: ",','.join(ext_req_checked.filename))
+    #print("In check_I1_I2_fastq(). Following files for lane: ", lane," passed: ",','.join(ext_req_checked.filename))
     return(ext_req_checked)
 
 def check_raw_4_file_format_techniques(file_list, manifest, aliquot):
