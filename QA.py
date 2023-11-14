745ee4e0e735dfc53db9df114dd9a0af1f014465
apaala.chatterjee <apaala.chatterjee@apaalam1-osx.local>
2023-11-14 16:33:47 -0500

    incorporating log feedback

--- QA.py
+++ QA.py
@@ -545,9 +545,10 @@ def check_tech_assoc_files(manifest, file_list, techniques, missing_files):
 def check_QA_for_aliquot(check_raw_files):
     req = None
     opt = None
+    ['None' if v is None else v for v in check_raw_files]
     if (~check_raw_files['Opt']).any() == False:
         opt = "FAILED"
-    elif (check_raw_files['Opt']).all() == True or (check_raw_files['Opt']).any() == None and (~check_raw_files['Opt']).any():
+    elif (check_raw_files['Opt']).all() == True or (check_raw_files['Opt']).any() == 'None' and (~check_raw_files['Opt']).any():
         opt = "PASSED"
     if not check_raw_files['Req'].all():
         req = "FAILED"
